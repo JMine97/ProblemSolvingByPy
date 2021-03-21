e62f83d3c1944abf53038db1a4ab77e38ffaeb96
Sangwoo Bae <42166742+Bae-sangwoo@users.noreply.github.com>
2021-03-21 20:23:19 +0900

    Update 1034.py
    fin

--- week1/bsw/1034.py
+++ week1/bsw/1034.py
@@ -6,7 +6,6 @@ K = int(input())
 ans =0
 for n in range(N):
     sameCol = 0
-    #print(arr[n].count('0'))
     if arr[n].count('0') <= K and arr[n].count('0')%2 == K%2:
         for i in range(N):
             if arr[n] == arr[i]:
@@ -23,13 +22,8 @@ print(ans)
 # 1 0 1 1 
 # 0 0 0 0 
 
-# 2
-# 3
-# 3
-# 1
-# 4
-
 '''''''''''''''''''''''
-
-
+1. 행의 모든 숫자가 같아야 동시에 정답이 될 수 있다
+2. 각 행의 0의 갯수와 K가 동시에 홀수이거나 짝수여야 한다
+3. 1과 2를 모두 만족하는 행의 최대 갯수를 출력
 '''''''''''''''''''''''
