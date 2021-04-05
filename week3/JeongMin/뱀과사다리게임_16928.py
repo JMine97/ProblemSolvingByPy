6a93aee392bc2e45d07b1304fe3f3943ddb263ef
JMine97 <52027965+JMine97@users.noreply.github.com>
2021-04-05 13:08:00 +0900

    Update 뱀과사다리게임_16928.py

--- week3/JeongMin/뱀과사다리게임_16928.py
+++ week3/JeongMin/뱀과사다리게임_16928.py
@@ -1,32 +1,39 @@
 import sys
 from heapq import heappop, heappush
-input=sys.stdin.readline
 
-n, m=map(int, input().split()) #사다리, 뱀
+input = sys.stdin.readline
 
-ladder=[]
-snake=[]
-graph=[0]*101
-for _ in range(n+m):
-    a, b=map(int, input().split())
-    graph[a]=b
+n, m = map(int, input().split())  # 사다리, 뱀
+
+ladder = []
+snake = []
+graph = [0] * 101
+for _ in range(n + m):
+    a, b = map(int, input().split())
+    graph[a] = b
 
 def bfs():
-    q=[]
+    q = []
     heappush(q, (0, -1))
+    flag = False
 
     while q:
-        cnt, start=heappop(q)
-        start=-start
-        if start==100:
-            print(cnt); return
-        for next in range(1, 6+1):
-            ns=start+next
-            if ns<=100 and graph[ns]!=-1:
-                if graph[ns] != 0:
-                    ns=graph[ns]
-                heappush(q, (cnt+1,-ns))
-                graph[ns]=-1
+        cnt, start = heappop(q)
+        start = -start
+        # print(q)
+        if flag==True:
+            print(cnt+1)
+            return
+        for next in range(1, 6 + 1):
+            ns = start + next
+            if ns <= 100 and graph[ns] != -1:
+                if graph[ns] > 0:
+                    ns = graph[ns]
+                if ns==100:
+                    flag=True
+                    break
+                heappush(q, (cnt + 1, -ns))
+                graph[ns] = -1
 
 bfs()
 
