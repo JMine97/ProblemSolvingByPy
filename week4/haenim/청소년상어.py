103eb93b12f685e64523ce1e63e1b04960d901cc
haenim <dlgoslaek1@gmail.com>
2021-04-26 17:39:47 +0900

    add new game

--- week4/haenim/청소년상어.py
+++ week4/haenim/청소년상어.py
@@ -5,8 +5,9 @@
     
     접근 방법:
 
-    현재 상어가 먹을 수 있는 물고기 중에서 번호가 가장 높은 것을 먹는다고 해서
-    그게 끝까지 최댓값이 된다고 확신할 수 없다.
+    현재 상어가 먹을 수 있는 물고기 중에서 번호가 가장 높은 것 만을 먹었을 때 최대가 되나?
+
+    => 현재 번호가 가장 높은 것을 먹는다고 해서 그게 끝까지 최댓값이 된다고 확신할 수 없다.
     
     => 모든 경우에 대해서 끝까지 탐색이 필요하다
 
@@ -17,16 +18,13 @@
     진행 과정
 
     1. 현재 칸에 있는 물고기를 먹는다
-    2. 1번 물고기부터 16번 물고기 까지 순서대로 물고기들이 움직인다
+    2. 1번 부터 16번 물고기 까지 순서대로 물고기들이 움직인다
     3. 이동할 방향에 상어가 있거나 존재하지 않는 칸이면 45도 반시계 회전하고 움직인다
-    4. 상어는 현재 방향으로 움직이면서 물고기를 먹는다 (한번에 여러칸 이동도 가능)
+    4. 상어는 현재 방향으로 움직이면서 물고기를 먹는다 (한 번에 여러칸 이동도 가능)
     
 '''
 
 import copy
-
-
-
 
 
 # 화살표 방향
@@ -44,21 +42,22 @@ def find_fish(array, index):
 def move_fish(lst, x, y):
     position = []
 
-    #1번 물고기부터 16번 물고기까지 움직임    
+    #모든 물고기들을 움직임    
     for i in range(1, 17):# 1번 물고기부터 16번 물고기 까지 반복
 
+        # i번 물고기의 위치 찾기
         position = find_fish(lst,i)
 
-        if position == None: # 현재 물고기가 없으면
+        if position == None: # i번 물고기가 없으면
             continue # 다음 물고기를 움직임
 
-        x1, y1 = position[0], position[1]
-        direct = lst[x1][y1][1] # 현재 물고기의 방향
+        x1, y1 = position[0], position[1] # i번 물고기의 위치 저장
+        direct = lst[x1][y1][1] # i번 물고기의 방향
 
         for j in range(8): # 각 방향에 대하여
-            nx, ny =  x1 + dx[direct], y1 + dy[direct] # 이동할 위치5
+            nx, ny =  x1 + dx[direct], y1 + dy[direct] # 이동할 위치
 
-            if 0 <= nx < 4 and 0 <= ny <4:# 이동할 위치가 범위 내에 있고
+            if 0 <= nx < 4 and 0 <= ny <4: # 이동할 위치가 범위 내에 있고
                     if not (nx == x and ny == y): # 다음으로 이동할 위치에 상어가 있는 게 아니면
 
                         # 현재 칸과 이동할 칸의 물고기의 위치를 서로 바꿈
@@ -73,20 +72,20 @@ def move_fish(lst, x, y):
 
 
 def find_possible_shark_pos(lst, x, y):
-    positions = []
+    positions = [] # 상어가 이동할 수 있는 모든 위치를 저장
     direction = lst[x][y][1] # 현재 상어의 방향
 
     for i in range(1,4):
         nx, ny = x + dx[direction], y + dy[direction] # 상어가 다음으로 이동할 위치
 
-        if 0 <= nx < 4 and 0 <= ny < 4 and 1 <= lst[nx][ny][0] <= 16: #다음으로 이동할 위치가 범위 내에 있고 거기에 물고기가 존재한다면
+        if 0 <= nx < 4 and 0 <= ny < 4 and 1 <= lst[nx][ny][0] <= 16: # 다음으로 이동할 위치가 범위 내에 있고 거기에 물고기가 존재한다면
             positions.append([nx,ny]) # 이동할 수 있는 위치에 추가
 
         x, y = nx, ny # 현재 위치를 이동한 위치로 바꿈
         
     return positions #상어가 이동할 수 있는 모든 위치를 전달
     
-def solution(lst, x, y, total):
+def dfs(lst, x, y, total):
     global answer
     
     lst = copy.deepcopy(lst) #원본 데이터를 바꾸지 않기 위해 딥카피
@@ -101,23 +100,50 @@ def solution(lst, x, y, total):
     # 4. 현재 위치에서 상어가 이동할 수 있는 모든 위치 찾기
     possible_shark_pos = find_possible_shark_pos(lst, x, y)
 
-    # 현재 최댓값과 새로운 최댓값을 비교해서 물고기 번호의 합이 높은 걸 답으로 선택
+    # 현재까지의 최댓값과 지금의 물고기 번호 합을 비교해서 물고기 번호의 합이 높은 걸 답으로 업데이트
     answer = max(answer, total + fish_number) 
         
     # 5. 상어가 이동할 수 있는 모든 위치에 대해서 최댓값 탐색
     for nx, ny in possible_shark_pos :
-        solution(lst, nx, ny, total + fish_number)
-#입력
-tmp = [list(map(int, input().split())) for _ in range(4)]
+        dfs(lst, nx, ny, total + fish_number)
 
+
+        
+# 입력
+tmp = [list(map(int, input().split())) for _ in range(4)]
+'''
+4 x 8
+
+16 7 1 4 4 3 12 8
+
+14 7 7 6 3 4 10 2
+
+5 2 15 2 8 3 6 4
+
+11 8 2 4 13 5 9 4
+
+'''
+
+# 다루기 쉽게 [물고기 번호, 방향] 형태로 바꿈
 lst = [[None]*4 for _ in range(4)]
 
-# 다루기 쉽게 [값, 방향] 형태로 바꿈
 for i in range(4):
     for j in range(4):
-                                                     # 0 1  / 2 3 / 4 5 / 6 7    
-        lst[i][j] = [tmp[i][j*2], tmp[i][j*2 +1] -1] # 1 2 / 3 4 / 5 6 / 7 8
+                                                     #  0 1  / 2 3 / 4 5 / 6 7    
+        lst[i][j] = [tmp[i][j*2], tmp[i][j*2 +1] -1] # 16 7 / 1 4 / 4 3 / 12 8
 
-answer = 0
-solution(lst, 0, 0, 0)
+
+'''
+[ [[16, 6], [1, 3], [4, 2], [12, 7]],
+
+  [[14, 6], [7, 5], [3, 3], [10, 1]],
+
+  [[5, 1], [15, 1], [8, 2], [6, 3]],
+
+  [[11, 7], [2, 3], [13, 4], [9, 3]]  ]
+
+'''
+
+answer = 0 # 물고기 번호의 최댓값
+dfs(lst, 0, 0, 0) # 리스트, x좌표, y좌표, 현재 먹은 물고기 번호 총 합
 print(answer)
---
+++ week5/lhn/새로운게임.py
@@ -0,0 +1,66 @@
+import sys
+
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
+def move(chess_num):
+    x, y, z = chess[chess_num] #num번째 턴에 움직일 체스
+
+    #입력 받은 말과 말이 있는 곳의 맨밑의 말 번호가 다르면 return
+    if chess_num != chess_map[x][y][0]:
+        return 0
+    
+    #다음으로 이동할 칸
+    nx = x + dx[z]
+    ny = y + dy[z]
+
+    #다음으로 이동했을 때 범위내에 없거나 파란색이면
+    if not 0 <= nx < n or not 0 <= ny < n or a[nx][ny] == 2:
+        #방향을 전환함
+        
+        if 0 <= z <= 1: #-> <- 방향으로 움직이는 경우에는
+            nz = (z+1) % 2 #각각 <- -> 방향으로 바꿔줌
+        else:
+            nz = (z-1) % 2 + 2 #위면 아래 아래면 위로 바꿈
+        chess[chess_num][2] = nz
+        nx = x + dx[nz]
+        ny = y + dy[nz]
+        #방향을 전환해도 움직일 수 없으면 끝냄
+        if not 0 <= nx < n or not 0 <= ny < n or a[nx][ny] == 2:
+            return 0
+
+    chess_set = []
+    chess_set.extend(chess_map[x][y]) #다음으로 이동할 체스 묶음
+    chess_map[x][y] = [] #현재 좌표에서 체스를 없앰
+
+    if a[nx][ny] == 1: #다음으로 이동할 칸이 빨간색이면
+        chess_set = chess_set[-1::-1] #말의 업힌 순서 변경
+
+    for i in chess_set:
+        chess_map[nx][ny].append(i) #체스 판에 말을 추가
+        chess[i][:2] = [nx, ny]
+
+    if len(chess_map[nx][ny]) >= 4: #4개 이상 올라탔으면 flag=1로
+        return 1
+    return 0
+
+n, k = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
+chess_map = [[[] for _ in range(n)] for _ in range(n)]
+chess = [0 for _ in range(k)]
+
+for i in range(k):
+    x, y, z = map(int, input().split())
+    chess_map[x-1][y-1].append(i)
+    chess[i] = [x-1, y-1, z-1]
+
+cnt = 1
+while cnt <= 1000:# 게임 턴이 1000 이하여야함
+    for i in range(k):
+        flag = move(i)
+        
+        if flag: # 이 턴에 게임이 끝났으면
+            print(cnt) #답 출력
+            sys.exit()
+    cnt += 1
+print(-1)
