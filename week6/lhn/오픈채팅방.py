70da59b8b72d4a69f1772b7099ee5f5e25bac8d8
haenim <dlgoslaek1@gmail.com>
2021-05-03 05:13:09 +0900

    add 오픈패팅방

---
+++ week6/lhn/오픈채팅방.py
@@ -0,0 +1,29 @@
+record = list(input().replace("[","").replace("]","").split(","))
+
+def solution(record):
+    answer = []
+    temp = []
+    temp2 = []
+    user = dict()
+    
+    for i in range(len(record)):
+        keyword = record[i].replace('"',"").split()
+
+        if keyword[0] == "Leave":
+            temp2.append("님이 나갔습니다.")
+            temp.append(keyword[1])
+            
+        elif keyword[0] == "Enter":
+            user[keyword[1]] = keyword[2]
+            temp.append(keyword[1])
+            temp2.append("님이 들어왔습니다.")
+   
+        else :
+            user[keyword[1]] = keyword[2]
+
+    for i in range(len(temp)):
+        answer.append(user[temp[i]]+temp2[i])
+        
+            
+    return answer 
+
