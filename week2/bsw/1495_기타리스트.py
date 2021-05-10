e6a0a0aa10b3924712f99ced30acbc92cdff2652
sangbae <bsw0111@naver.com>
2021-05-10 18:30:53 +0900

    기타리스트 메모리 초과

--- week2/bsw/1495_기타리스트.py
+++ week2/bsw/1495_기타리스트.py
@@ -1,21 +1,23 @@
-
-#개수N 시작볼륨S 최대볼륨M
+# 메모리초과
+# 개수N 시작볼륨S 최대볼륨M
 N, S, M = 3, 5, 10
 V = [5, 3, 7]
 
-
-from collections import deque
+N, S, M = map(int, input().split())
+V = list(map(int, input().split()))
 
 q=[S]
-
 for v in V:
     tmp = []
+    if not q:
+        q = [-1]
+        break
     for q_ in q:
         if q_ + v <= M:
             tmp.append(q_+v)
         if q_ - v >= 0:
             tmp.append(q_-v)
-    q=tmp
+    q=[t for t in tmp]
 
 
-print(q)
+print(max(q))
