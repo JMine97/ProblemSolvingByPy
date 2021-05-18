1b6512d3812f1c1f4a54bf540f90ce7e5c87dbcc
sangbae <bsw0111@naver.com>
2021-05-18 20:55:26 +0900

    add 1753

---
+++ week8/bsw/1753_최단경로.py
@@ -0,0 +1,36 @@
+V, E = map(int, input().split())
+s = int(input())
+
+# 시간초과
+
+graph = {}
+for _ in range(E):
+    start, end, cost = map(int, input().split())
+
+    if start not in graph:
+        graph[start] = [(end, cost)]
+    else:
+        graph[start].append((end, cost))
+
+distance = [999999999]*(V+1)
+distance[s] = 0
+
+from collections import deque
+q=deque([s])
+# print(graph)
+# print(distance)
+while q:
+    cur = q.popleft()
+
+    if cur not in graph:
+        continue
+    for next, cost in graph[cur]:
+
+        distance[next] = min(distance[next], distance[cur] + cost)
+        q.append(next)
+
+for i in range(1, V+1):
+    if distance[i] == 999999999:
+        print('INF')
+    else:
+        print(distance[i])
