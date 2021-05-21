a0026ab6466e5a6ee13f7e80a593022fe3c98f90
sangbae <bsw0111@naver.com>
2021-05-21 16:46:39 +0900

    update 1759

--- week9/bsw/1759_암호 만들기.py
+++ week9/bsw/1759_암호 만들기.py
@@ -2,14 +2,17 @@
 알파벳 L개
 모음최소 1개 자음최소 2개
 오름차순
+중복없음
 
+
+모든 경우의 수 에서 모음1개미만, 자음 2개미만의 경우를 제외
 '''
 
 L, C = map(int, input().split())
 arr = list(input().split())
 from itertools import combinations
 
-comb_arr = list(combinations(sorted(arr), 4))
+comb_arr = list(combinations(sorted(arr), L))
 
 mo = set(['a', 'e', 'i', 'o', 'u'])
 for comb in comb_arr:
