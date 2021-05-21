a1379bca284fc00d7d9b532abd35d693b51a8578
sangbae <bsw0111@naver.com>
2021-05-21 19:50:16 +0900

    add 4485.py solved by DFS

---
+++ week9/bsw/4485_녹색 옷 입은 애가 젤다지.py
@@ -0,0 +1,41 @@
+# DFS 시간초과 안나옴....
+
+cnt=1
+while 1:
+    N = int(input())
+    if N == 0:
+        break
+    
+    graph=[]
+    for _ in range(N):
+        graph.append(list(map(int, input().split())))
+
+    # sol
+    from heapq import heappop, heappush
+    pq = []
+    #heappush(pq, (graph[0][0], 0, 0))
+    pq.append((0,0))
+    distance = [[999999999] * N for _ in range(N)]
+    distance[0][0] = graph[0][0]
+    
+    while pq:
+        #cost, x, y = heappop(pq)
+        x, y = pq.pop()
+        for dy, dx in (1,0), (0,1), (-1, 0), (0,-1):
+            nx, ny = x+dx, y+dy
+
+            if N<=nx or nx<0 or N<=ny or ny<0:
+                continue
+            
+            if distance[nx][ny] > distance[x][y] + graph[nx][ny]:
+                distance[nx][ny] = distance[x][y]+graph[nx][ny]
+                #heappush(pq, (distance[nx][ny],nx,ny))
+                pq.append((nx,ny))
+            
+                
+
+    print("Problem {}: {}" .format(cnt, distance[N-1][N-1]))
+    cnt+=1
+
+    #print(distance)
+    
