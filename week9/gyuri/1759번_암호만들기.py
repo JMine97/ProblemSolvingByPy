08e153e13f87889f4f7b121516e8cf4b42d2990a
kinggyu <63337662+KimGyuri875@users.noreply.github.com>
2021-05-22 11:03:05 +0900

    Update 1759번_암호만들기.py

--- week9/gyuri/1759번_암호만들기.py
+++ week9/gyuri/1759번_암호만들기.py
@@ -13,8 +13,9 @@ l, c = map(int, input().split())
 pw = list(map(str, input().split()))
 pw.sort()
 vowel = ['a', 'e', 'i', 'o', 'u']
-# 모음이 몇개 있는지 골라내기
+
 answer = list(combinations(pw, l))
+
 check = set()
 for a in answer:
     count_v = 0
@@ -24,7 +25,8 @@ for a in answer:
     if count_v < 1 or len(a) - count_v < 2:
         continue
     check.add(a)
-check = list(check)
-check.sort()
+    
+check = sorted(list(check))
+
 for i in check:
     print(''.join(i))
