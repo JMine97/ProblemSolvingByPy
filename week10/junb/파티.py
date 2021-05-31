a39a37b1fd5cb171bc1fb6bedc75b53b73430302
Junbeom Wi <minkwns@naver.com>
2021-05-31 14:13:27 +0900

    update file

--- week10/junb/파티.py
+++ week10/junb/파티.py
@@ -8,7 +8,6 @@ input = sys.stdin.readline
 n, m, x = map(int, input().split())
 graph = [[] for _ in range(n+1)]
 result = [0]*(n+1)
-dist = [INF] *(n+1)
 for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a].append((c, b)) # cost, end 
