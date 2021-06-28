65ce3dedecfbd61f76a9d25033a7fed83c9bf394
sangbae <bsw0111@naver.com>
2021-06-28 20:08:52 +0900

    Update 9935

--- week14/bsw/9935_문자열 폭발.py
+++ week14/bsw/9935_문자열 폭발.py
@@ -1,10 +1,11 @@
+# 시간초과
+# 왜?
+
 string = input()
 bomb = input()
 
 answer = ''
 
-string = 's'*500000 + '4'*500000
-bomb = 'ssssssssssssssssss444444444444444444'
 for i in range(len(string)):
     answer+=string[i]
     if string[i] == bomb[-1]:
